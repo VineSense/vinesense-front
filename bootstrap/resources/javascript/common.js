6d27b6f1d09af590ad2d6b83e50975a5cda08c36
Enterkey <enterkey88@gmail.com>
2015-02-11 16:28:31 -0500

    panel window 크기 조절 되는 기능 추가

---
+++ bootstrap/resources/javascript/common.js
@@ -0,0 +1,73 @@
+if ( typeof (vinesense) == typeof (undefined)) {
+    vinesense = {};
+}
+
+vinesense = {
+    selectPanelHandler : {
+        'chart-1' : function(e) {
+            var mainPanel = $('#chart-1-panel'),
+                sidePanel = $("#chart-1-side-panel"),
+                mainSpan = $('#chart-1-span');
+
+            vinesense.togglePanelSpanClass(mainPanel, mainSpan, sidePanel);
+        },
+        'map' : function() {
+            var mainSpan = $('#map-span'),
+                mainPanelBody = $('#map-panel .panel-body');
+            if(mainSpan.attr('class').indexOf('glyphicon-resize-full') != -1) {
+                mainSpan.removeClass('glyphicon-resize-full');
+                mainSpan.addClass('glyphicon-resize-small');
+                mainPanelBody.show();
+
+            } else {
+                mainSpan.removeClass('glyphicon-resize-small');
+                mainSpan.addClass('glyphicon-resize-full');
+                mainPanelBody.hide();
+            }
+
+        },
+        'chart-2' : function() {
+            var mainPanel = $('#chart-2-panel'),
+                sidePanel = $("#chart-2-side-panel"),
+                mainSpan = $('#chart-2-span');
+
+            vinesense.togglePanelSpanClass(mainPanel, mainSpan, sidePanel);
+        }
+    },
+
+    init : function() {
+      this.addEventListener();
+    },
+
+    addEventListener : function() {
+        this.togglePanelWindow();
+    },
+
+    togglePanelWindow : function() {
+        $('[data-event-change-panel]').on('click', function () {
+            var selectedValue = $(this).attr('data-event-change-panel');
+            vinesense.selectPanelHandler[selectedValue]();
+        });
+    },
+
+    togglePanelSpanClass : function(mainPanel, mainSpan, sidePanel) {
+        if(mainPanel.attr('class') == 'col-lg-8') {
+            mainPanel.removeClass('col-lg-8');
+            mainPanel.addClass('col-lg-12')
+            mainSpan.removeClass('glyphicon-resize-full');
+            mainSpan.addClass('glyphicon-resize-small');
+            sidePanel.hide();
+
+        } else {
+            mainPanel.removeClass('col-lg-12');
+            mainPanel.addClass('col-lg-8')
+            mainSpan.removeClass('glyphicon-resize-small');
+            mainSpan.addClass('glyphicon-resize-full');
+            sidePanel.show();
+        }
+    }
+}
+
+$(document).on('ready page:load', function() {
+    vinesense.init();
+});
